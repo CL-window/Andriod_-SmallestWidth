2a9dddb4b463e64a5b16830437b5e16d559dfaae
CL-window <704065100@qq.com>
2020-05-22 16:13:50 +0800

    Update SWUtils.java

--- SWUtils.java
+++ SWUtils.java
@@ -14,6 +14,32 @@ import java.text.DecimalFormat;
  * density = DPI / 160 = 480/160=3
  * 屏幕的总 dp 宽度 = 屏幕的总 px 宽度 / density =1080/3=360
  * 
+ * 
+ * 
+ * OPPO A37M: 720x1280, 320            - 360  
+ * OPPO A57: 720x1280, 320             - 360  
+ * 小米5: 1080x1920, 480                - 360
+ * 小米CC9 Pro: 1080x2340, 440          - 392
+ * 小米10 Pro: 1080x2340, 440           - 392
+ * 小米8 SE : 1080x2244, 440            - 392
+ * 小米CC9e:720x1560, 320               - 360
+ * 小米MIX2 : 1080x2160, 440            - 392
+ * 华为荣耀9X: 1080x2340, 480            - 360
+ * 华为nova4: 1080x2310, 480            - 360
+ * 锤子坚果pro 2s: 1080x2160, 400       - 432
+ * nubia X: 1080x2280, 480             - 360
+ * 金立GN3001: 720x1280, 320            - 360 
+ * 一加 OnePlus 7 Pro:1440x3120, 560    - 411
+ * 美图M6/T8/T9: 1080x1920, 480         - 360
+ * LG Nexus 5X: 1080x1920, 420         - 411
+ * 魅族 16th: 1080x2160, 480            - 360
+ * 三星 Galaxy Note4: 1440x2560, 640    - 360
+ * 华为Mate 20 Pro: 1440x3120, 640      - 360
+ * 诺基亚NOKIA TA-1131: 1080x2246, 420   - 411
+ * vivo X30: 1080x2400, 480             - 360
+ * 华为P40 Pro: 1200x2640, 480          - 400
+ * vivo X30: 1080x2400, 480             - 360
+ * 酷派 Coolpad Y75                     - 360
  */
 public class SWUtils {
 
@@ -26,8 +52,14 @@ public class SWUtils {
      // 设计的标准尺寸
     private final float DESIGN_SIZE = 360.0f;
 
-    // 需要适配的所有尺寸
-    private final int[] SW_LIST = new int[]{360, 380, 400, 420, 440, 460, 480, 600};
+    // 需要适配的所有尺寸，基本可以适配市面上的大部分机型
+    private final int[] SW_LIST = new int[]{360, 390, 400, 410, 430, 480, 500, 600, 720, 800};
+    // 如果担心适配不全，可以每隔20设置一个档位
+    // private final int[] SW_LIST = new int[]{360, 380, 390, 400, 410, 420, 430, 440, 460, 480, 
+    //     500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700, 720, 740, 760, 780, 800,
+    //     820, 840, 860, 880, 900, 920, 940, 960, 980, 1000, 1020, 1040, 1060, 1080, 1100,
+    //     1120, 1140, 1160, 1180, 1200, 1220, 1240, 1260, 1280, 1300};
+    // private final int[] SW_LIST = new int[]{480};
 
     // 最大适配到的数据, 比如最大适配到500dp
     private final int MAX_DP = 600;
@@ -45,9 +77,9 @@ public class SWUtils {
         float result = src / DESIGN_SIZE * value;
         String formatResult = decimalFormat.format(result);
         String info = "    <dimen name=\"" + DIMEN_ITEM_NAME_PRE + name + "dp\">" + formatResult + "dp</dimen>\n";
-        if (DEBUG) {
-            System.out.println(info);
-        }
+        // if (DEBUG) {
+            // System.out.println(info);
+        // }
         return info;
     }
 
@@ -59,10 +91,10 @@ public class SWUtils {
 
         File dir = new File(getClass().getResource("/").getPath());
         File outDir = new File(dir, "outputs");
-
-        StringBuilder outputs = new StringBuilder();
+        deleteDir(outDir);
 
         for (int i = 0; i < SW_LIST.length; i++) {
+            StringBuilder outputs = new StringBuilder();
             outputs.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
             outputs.append("<resources>\n");
             int cur = SW_LIST[i];
@@ -71,7 +103,7 @@ public class SWUtils {
             File swFile = new File(swDir, "dimens.xml");
 
             outputs.append(getValue(cur, 0.5f, "0_5"));
-            for (int j = 1; j <= MAX_DP; j++) {
+            for (int j = 0; j <= MAX_DP; j++) {
                 outputs.append(getValue(cur, j, j+""));
             }
 
@@ -79,6 +111,22 @@ public class SWUtils {
             writeFile(swFile, outputs.toString());
         }
 
+    }
+
+    private void deleteDir(File dir) {
+        if (dir.exists()) {
+            if (dir.isFile()) {
+                dir.delete();
+                if (DEBUG) {
+                    System.out.println("Delete " + dir.getAbsolutePath() + " Success");
+                }
+            } else {
+                File[] files = dir.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    deleteDir(files[i]);
+                }
+            }
+        }
     }
 
     private void writeFile(File file, String info) {
